9640b8bbafe643e9684eb31281f9e4bc4596d0ad
Oleg <klaus888@yandex.ru>
2023-03-04 00:59:37 +0300

    time work

--- start.py
+++ start.py
@@ -1,20 +1,22 @@
-import time
+from datetime import datetime
 from tkinter import *
 from tkinter import ttk
 
 
-class Timer():
-    def __init__(self, subscribe, start):
-        self.subscribe = subscribe
-        self.start = start
-        
-    def stop(self):
-        print(self.start + 'stop')
-        return self.start + 'stop'
+start_t = 0
+stop_t = 0
 
 def start():
-    start_time = time.time()
+    global start_t
+    start_t = datetime.now()
+    str_start = start_t.strftime('%H:M:%S')
+    work_label['text'] = 'Начало:', str_start, subscribe.get()
 
+def stop():
+    stop_t = datetime.now()
+    print (start_t, stop_t)
+    delta_t = stop_t - start_t
+    work_label['text'] = f'{delta_t} ушло на выполнение задачи: {subscribe.get()}'
 
 
 root = Tk()
@@ -22,14 +24,12 @@ root.title('Timer for work')
 
 content = ttk.Frame(root)
 
-s = Label(root, text='Описание работы')
-s.grid(row=0, columnspan=5)
+l1 = Label(root, text='Описание работы')
+l1.grid(row=0, columnspan=5)
 
-subscribe = StringVar()
-ent = ttk.Entry(root, textvariable=subscribe, width=100)
-ent.grid(row=1, columnspan=5, pady=3)
+subscribe = ttk.Entry(root, width=100)
+subscribe.grid(row=1, columnspan=5, pady=3)
 
-start_time = IntVar()
 start_btn = ttk.Button(root, text='START', width=40, command=start)
 start_btn.grid(column='0', row='2', columnspan=2, sticky=(W), pady=3)
 pause_btn = ttk.Button(root, text='PAUSE')
@@ -38,6 +38,8 @@ pause_btn.grid(column='2', row='2')
 pause_btn = ttk.Button(root, text='STOP', width=40, command=stop)
 pause_btn.grid(column='3', row='2', columnspan=2,sticky=(E))
 
+work_label = ttk.Label(root, text='Начало:')
+work_label.grid(column=0, row=4, sticky=(W))
 
-https://www.delftstack.com/ru/howto/python-tkinter/how-to-use-a-timer-in-tkinter/
+
 root.mainloop()
