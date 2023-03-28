6c88aabdafa6b091f70db8565efbae4935a727c7
Oleg <klaus888@yandex.ru>
2023-03-29 00:57:34 +0300

    1

--- start_cl.py
+++ start_cl.py
@@ -1,73 +1,93 @@
 import tkinter as tk
 from datetime import datetime
 from tkinter import IntVar, StringVar, ttk
+import json
+
+
+def saver(timers_list: list):
+    with open('timers.txt', mode='a') as file:
+        str_to_file = ''
+        for _ in timers_list:
+            str_to_file += f'Задание: {_.text}, время: {_.result_time}\n'
+
+            file.write(str_to_file)
 
 
 class Window(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
-        # self.title("timer for work")
-        # self.geometry("500x300")
         self.obj_list = []
 
-        self.l1 = ttk.Label(text="about worktack").grid(row=0, column=0)
+        self.l1 = ttk.Label(text="Worktack")
+        self.l1.grid(
+            row=0, column=0, columnspan=5)
 
-        self.text = ttk.Entry()
-        self.text.grid(row=1, column=0)
+        self.ent_text = ttk.Entry(width=50)
+        self.ent_text.grid(row=1, column=0, columnspan=5, sticky=('EW'))
 
-        self.button_start = ttk.Button(
+        self.btn_start = ttk.Button(
             text="start", command=self.start_timer
-        ).grid(row=2, column=0)
+        )
+        self.btn_start.grid(row=2, column=0, columnspan=2, sticky="EW")
 
-        self.button_pause = ttk.Button(
-            text="pause", command="", default="disable"
-        ).grid(row=2,column=1)
+        self.btn_pause = ttk.Button(
+            text="pause", command="", state='disable'
+        )
+        self.btn_pause.grid(row=2, column=2)
 
-        self.button_stop = ttk.Button(
-            text="stop", command=self.stop_timer)
-        self.button_stop.grid(row=2,column=2)
+        self.btn_stop = ttk.Button(
+            text="stop", command=self.stop_timer, state='disable')
+        self.btn_stop.grid(row=2, column=3, columnspan=2,  sticky="EW")
 
         self.current_text = StringVar()
         self.current_text.set("---------")
         self.current_task = ttk.Label(
-            self, textvariable=self.current_text, foreground="red"
+            textvariable=self.current_text, foreground="red"
         )
-        self.current_task.grid()
+        self.current_task.grid(row=3, column=0, columnspan=3)
 
         self.all_tasks_text = StringVar()
         # self.all_tasks_text.set('---------')
         self.all_tasks = ttk.Label(
-            self, textvariable=self.all_tasks_text).grid()
+            textvariable=self.all_tasks_text)
+        self.all_tasks.grid(row=4, column=0, columnspan=4)
 
-        self.a = 0
         self.counter = StringVar()
-        self.counter.set("-------------------")
-        self.count_lab = ttk.Label(self, textvariable=str(self.counter)).grid()
+        self.counter.set("---")
+        self.count_lab = ttk.Label(textvariable=str(self.counter), font='bold')
+        self.count_lab.grid(row=3, column=4, columnspan=4)
 
     def count(self):
-        self.counter.set(self.delta_time(self.timer_obj.start))
-        self.after(1000, self.count)
+        current_time = self.delta_time(self.timer_obj.start)
+        self.counter.set(current_time)
+        self.timer_obj.result_time = current_time
+        self.after(100, self.count)
 
     @staticmethod
     def delta_time(start):
-        delta_time = datetime.now() - start
-        return delta_time
+        delta_time = str(datetime.now() - start)
+        return delta_time.split('.')[0]
 
     def start_timer(self):
         self.timer_obj = Timer()
-        self.timer_obj.text = self.text.get()
+        self.timer_obj.text = self.ent_text.get()
         self.obj_list.append(self.timer_obj)
         self.current_text.set(
             f"Задание {self.timer_obj.text}, старт: {self.timer_obj.start.strftime('%H:%M')}"
         )
         self.count()
-        self.button_stop.config(state='disable')
-        print(self.button_stop)
+        self.btn_stop.config(state='normal')
+        self.btn_pause.config(state='normal')
+        self.btn_start.config(state='disable')
 
     def stop_timer(self):
-        self.timer_obj.result_time = self.delta_time(self.timer_obj.start)
+        self.btn_stop.config(state='disable')
+        self.btn_pause.config(state='disable')
+        self.btn_start.config(state='normal')
         self.show_all()
+        self.count_lab.config(state='disable')
+        saver(self.obj_list)
 
     def show_all(self):
         str_timers = ""
@@ -80,9 +100,9 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title('Temperature Converter')
-        self.geometry('300x70')
-        self.resizable(False, False)
+        self.title('Work timer')
+        self.geometry('500x500')
+        # self.resizable(False, False)
 
 
 class Timer:
